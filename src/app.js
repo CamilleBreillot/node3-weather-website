ef52939163f999b09b6ba1be912095d2b4c785b3
camille breillot <camille.breillot@gmail.com>
2022-02-01 18:44:28 +0100

    initial commit

---
+++ src/app.js
@@ -0,0 +1,88 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+
+const app = express()
+
+// define path for Express config
+const publicDirectory = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+//Setup static directory to serve
+app.use(express.static(publicDirectory))
+
+app.get('', (req, res) => {
+   res.render('index', {
+     title: 'Weather',
+     name: 'Camille Breillot'
+   })
+})
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About me',
+    name: 'Camille Breillot'
+  })
+})
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+    helpText: 'Need some help?',
+    title: 'Help',
+    name: 'Camille Breillot'
+  })
+})
+
+app.get('/weather', (req, res) => {
+
+  if (!req.query.address) {
+    return res.send({
+      error: 'you must provide address'
+    })
+  }
+
+  geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
+    if (error) {
+      return res.send({ error })
+    }
+    forecast(latitude, longitude, (error, forecastData) => {
+      if (error) {
+        return res.send({ error })
+      }
+
+      res.send({
+        address: req.query.address,
+        location,
+        forecast: forecastData
+      })
+    })
+  })
+})
+
+app.get('/help/*', (req, res) => {
+  res.render('404', {
+    title: '404 help',
+    name: 'Camille Breillot',
+    errorMessage: 'Help article not found'
+  })
+})
+
+app.get('*', (req, res) => {
+  res.render('404', {
+    title: '404',
+    name: 'Camille Breillot',
+    errorMessage: 'Page not found'
+  })
+})
+
+app.listen(3000, () => {
+  console.log('Server is up and running on port 3000')
+})
---
+++ src/utils/forecast.js
@@ -0,0 +1,19 @@
+const request = require('request')
+
+const forecast = (lat, long, callback) => {
+  const url = `http://api.weatherstack.com/current?access_key=7c45554a063761e40fb58d3d3a0de46e&query=${long},${lat}`
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback("Unable to reach network", undefined)
+    } else if (body.error) {
+      callback("Unable to find location", undefined)
+    } else {
+      const temp = body.current.temperature
+      const tempRessenti = body.current.feelslike
+      const weatherDescrip = body.current.weather_descriptions[0]
+      callback(undefined, `${weatherDescrip}. It is currently ${temp} degrees out. It feels like ${tempRessenti} degrees out.`)
+    }
+  })
+}
+
+module.exports = forecast
